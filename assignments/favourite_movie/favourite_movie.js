e59944528746feeb99202a9ad442c3c011d2aa15
suhail khazir bhat <suhailbhat7@gmail.com>
2022-03-03 09:59:55 +0530

    subb_js2

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,14 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if(operation==="add"){
+        movies.push(movie)
+    }
+    if(operation==="remove")
+    {
+        movies.pop(movie)
+    }
+    return movies
 }
 
 module.exports = favouriteMovie;
