b8b96385ff89041af72c775ae122b73a96a5a6ab
suhail khazir bhat <suhailbhat7@gmail.com>
2022-03-15 11:40:29 +0530

    ass-done-js8

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,18 +1,37 @@
-let btn = document.querySelector('button')
-let id= 0;
-btn.addEventListener("click", function (e)
-{
-    let todolist = document.querySelector('div.todos');
-    let input = document.querySelector('input');
-    let p = document.createElement("p");
-    p.innerHTML = input.value;
-    p.setAttribute("key", id);
-    todolist.appendChild(p)
-    input.value = ""
-    id += 1;
-    document.querySelector(`p[key="${id-1}"]`).addEventListener("click", function(e)
-    {
-        let todolist = document.querySelector('div.todos');
-        todolist.removeChild(this);
-    })
-});
+const compBtn = document.querySelector('button#comp');
+const pendBtn = document.querySelector('button#pend');
+async function fetchtodo(){
+    let res=await fetch('https://jsonplaceholder.typicode.com/todos');    
+    let data=await res.json();
+    console.log(data)
+    
+    compBtn.onclick = function(){  
+        let output=''; 
+    data.forEach(function(todo,index) {
+        if (todo.completed == true){ output+=`
+       <p style="color:blue;border: 2px solid powderblue;"key=${todo.id}>
+                "user-id":  ${todo.userId},<br>
+                "id": ${todo.id},<br>
+                "title": ${todo.title},<br> "completed":  ${todo.completed},<br>
+                </p>
+                     `       
+               
+    }})
+    document.querySelector('.todos').innerHTML=output;
+    }
+    pendBtn.onclick = function(){
+        let output='';
+        data.forEach(function(todo,index) {
+            if (todo.completed == false){
+            output+=`<p  style="color:blue;border: 2px solid powderblue;" key=${todo.id}>
+                  "userId":  ${todo.userId},<br>
+                   "id": ${todo.id},<br>
+                   "title": ${todo.title},<br>
+                  "completed":  ${todo.completed},<br>
+                 </p>`
+    }})
+        
+        document.querySelector('.todos').innerHTML=output;
+        }
+}
+    fetchtodo();
